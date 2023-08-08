01c8e8afd00751aec39d9e303d1d051affa7a864
Francis Reyes <reyesfrancisp@gmail.com>
2023-08-07 22:20:22 -0400

    fixed some errors

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,71 +1,114 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+const is_prod = process.env.NODE_ENV === 'production';
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    title: 'Webpack Example',
+    template: './src/main.html'
+  })
+];
+
+if (is_prod) {
+  plugins.push(new GenerateSW());
+  plugins.push(new WebpackPwaManifest({
+    name: 'Notes PWA App',
+    short_name: 'NPWAApp',
+    description: 'Amazing note taking application!',
+    background_color: '#555',
+    theme_color: '#35ae9b',
+    display: 'standalone',
+    inject: true,
+    publicPath: '/',
+    // crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+    icons: [
+      {
+        src: path.resolve('src/images/logo.png'),
+        sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+        ios: true
+      },
+      {
+        src: path.resolve('src/assets/images/logo_192.png'),
+        size: '192x192',
+        purpose: 'maskable'
+      }
+    ]
+  }))
+}
+
+module.exports = {
+  entry: './src/js/index.js',
+  mode: is_prod ? 'production' : 'development',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,
+    environment: {
+      arrowFunction: false,
+      bigIntLiteral: false,
+      const: false,
+      destructuring: false,
+      dynamicImport: false,
+      forOf: false,
+      module: false
     },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      // Add HtmlWebpackPlugin to generate HTML files
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        chunks: ['main'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './src/install.html',
-        filename: 'install.html',
-        chunks: ['install'],
-      }),
-      // Add WebpackPwaManifest to generate the manifest file
-      new WebpackPwaManifest({
-        name: 'Your App Name',
-        short_name: 'App Name',
-        description: 'Your app description',
-        background_color: '#ffffff',
-        theme_color: '#2196F3',
-        icons: [
-          {
-            src: path.resolve('src/icon.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
+    // publicPath: './' // set assets path to relative
+  },
+  devServer: {
+    compress: true,
+    port: 8000,
+    open: true,
+    hot: true,
+    watchFiles: ['./src/*.html']
+  },
+  plugins,
+  module: {
+    rules: [
+      // {
+      //   test: /\.css$/i,
+      //   use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      // },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
         ],
-      }),
-      // Add InjectManifest to include the service worker
-      new InjectManifest({
-        swSrc: './src/sw.js', // path to your service worker file
-        swDest: 'service-worker.js',
-      }),
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'ie 9' }]
+            ]
+          }
+        }
+      }
     ],
-    module: {
-      rules: [
-        // Add CSS loader to handle CSS files
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-        // Add Babel loader to transpile JavaScript files
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
-        },
-      ],
-    },
-  };
-};
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
+      new TerserPlugin()
+    ],
+  },
+}
